8beb125783286da04edbded71297e2ce8804352b
Colin Huliselan <colinhuliselan@ma-145-5-193-11.mobile-devices.eur.nl>
2020-01-22 13:48:27 +0100

    File now also includes predictions for a test set. You can use it to fill in values for users that do not appear in training also

--- BaselinePredictions.py
+++ BaselinePredictions.py
@@ -9,32 +9,92 @@ from sklearn import metrics
 observations = pd.read_csv('Observations_Report.csv', sep=';')
 game = pd.read_csv('Observations_Game.csv', sep=';')
 
-# %% BASELINE
+# %% GENERAL
 
 ## Note: Here I use the full observations set to compare the baseline predictions
 ## with the "real" values. In your case you probably substitute the observations
 ## dataframe with a test set
 
 ## Case 1: Majority rule (predict zero for everyone)
-## 1. MSE: 0.02192184495444662
+## 1. MSE: 0.0219218449544466
 baseline1 = [0]*observations.shape[0]
-metrics.mean_squared_error(observations['CLICK'], baseline1)
+RMSE1 = np.sqrt(metrics.mean_squared_error(observations['CLICK'], baseline1))
 
 ## Case 2: Use overall click rate for prediction
 ## 1. MSE 0.021441277668239916
 baseline2 = [np.mean(observations['CLICK'])]*observations.shape[0]
-metrics.mean_squared_error(observations['CLICK'], baseline2)
+RMSE2 = np.sqrt(metrics.mean_squared_error(observations['CLICK'], baseline2))
 
 ## Case 3: Use click rate per person as prediction using
 ## https://pandas.pydata.org/pandas-docs/stable/user_guide/groupby.html
 ## 1. MSE
 baseline3 = observations.groupby('USERID')['CLICK'].transform('mean')
-metrics.mean_squared_error(observations['CLICK'], baseline3)
+RMSE3 = np.sqrt(metrics.mean_squared_error(observations['CLICK'], baseline3))
 
 ## Case 4: Use click rate per offer as prediction
 ## 1. MSE
 baseline4 = observations.groupby('OFFERID')['CLICK'].transform('mean')
-metrics.mean_squared_error(observations['CLICK'], baseline4)
+RMSE4 = np.sqrt(metrics.mean_squared_error(observations['CLICK'], baseline4))
+
+## Print the results
+
+print(RMSE1)
+print(RMSE2)
+print(RMSE3)
+print(RMSE4)
+
+# %% HOW TO APPLY IN A TRAIN TEST SCENARIO
+from sklearn.model_selection import train_test_split
+
+## Some data with a train an test split (see TrainTestSmall.py)
+observations = pd.read_csv('Observations_Report.csv', sep=';')
+game = pd.read_csv('Observations_Game.csv', sep=';')
+seed = 1
+nObs = 1000000
+observationsSmall = observations.sort_values(by=['USERID'], axis = 0, ascending = True)[1:nObs]
+trainset, testset = train_test_split(observationsSmall, test_size = 0.2, random_state=seed)
+
+## Case 1: Majority rule (predict zero for everyone)
+## 1. MSE: 0.02192184495444662
+baseline1 = [0]*testset.shape[0]
+RMSE1 = np.sqrt(metrics.mean_squared_error(testset['CLICK'], baseline1))
+
+## Case 2: Use overall click rate for prediction
+## 1. MSE 0.021441277668239916
+baseline2 = [np.mean(trainset['CLICK'])]*testset.shape[0]
+RMSE2 = np.sqrt(metrics.mean_squared_error(testset['CLICK'], baseline2))
+
+## Case 3: Use click rate per person as prediction using
+## https://pandas.pydata.org/pandas-docs/stable/user_guide/groupby.html
+## 1. Find averages per user in training set
+temp = trainset.groupby('USERID')['CLICK'].mean().reset_index()
+## 2. Assign these averages to users in test set
+testsetInd = testset[['USERID','OFFERID']]
+baseline3 = testsetInd.merge(temp, how='left', on = 'USERID')['CLICK']
+## 3. Add the average click rate (in the train set) to missing users
+#print(baseline3.isnull().sum())
+baseline3 = baseline3.fillna(np.mean(trainset['CLICK']))
+## 4. Calculate RMSE
+RMSE3 = np.sqrt(metrics.mean_squared_error(testset['CLICK'], baseline3))
+
+## Case 4: Use click rate per offer as prediction using
+## https://pandas.pydata.org/pandas-docs/stable/user_guide/groupby.html
+## 1. Find averages per offer in training set
+temp = trainset.groupby('OFFERID')['CLICK'].mean().reset_index()
+## 2. Assign these averages to users in test set
+testsetInd = testset[['USERID','OFFERID']]
+baseline4 = testsetInd.merge(temp, how='left', on = 'OFFERID')['CLICK']
+## 3. Add the average click rate (in the train set) to missing users
+#print(baseline4.isnull().sum())
+baseline4 = baseline4.fillna(np.mean(trainset['CLICK']))
+## 4. Calculate RMSE
+RMSE4 = np.sqrt(metrics.mean_squared_error(testset['CLICK'], baseline4))
+
+print(RMSE1)
+print(RMSE2)
+print(RMSE3)
+print(RMSE4)
+
 
 ## Possible additional baseline methods.
 ## Case 5: Use the average of (1) avg user click rate and (2) avg offer click rate
