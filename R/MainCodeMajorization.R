b52d50b241fd97b9dbf5d65b40ff4f89e9f38882
Colin Huliselan <colinhuliselan@ma-145-5-214-222.mobile-devices.eur.nl>
2020-03-04 16:03:32 +0100

    random

--- R/MainCodeMajorization.R
+++ R/MainCodeMajorization.R
@@ -6,6 +6,7 @@ library(tictoc)
 library(RcppArmadillo)
 library(Rcpp)
 library(ggplot2)
+library(openxlsx)
 
 # sourceCpp("/Users/colinhuliselan/Documents/Master/Seminar/Seminar2020_V2/R/gammaui.cpp")
 #sourceCpp("~/Dropbox/Uni/Master_Econometrie/Blok_3/Seminar2020/R/gammaui.cpp")
@@ -262,6 +263,84 @@ test$prediction[is.na(test$prediction)] <- 0
 
 hist(test$prediction)
 
-# Problem with indices ------------------------------------------------------------------
+# Preparing data for mate ----------------------------------------------------------------
+# This is how you should import the data.
+# The sequence here is important. We want to have a continuous sequence, starting at 1
+# for the indices for user and order in our training set.
 
-df_test <- readRDS("/Users/colinhuliselan/Documents/Master/Seminar/Code/SeminarR/df_test")
+# Import train and game set from whereever you store them
+df_obs <- read_delim("/Users/colinhuliselan/Documents/Master/Seminar/Code/SeminarR/Observations_Report.csv",
+                       ";", escape_double = FALSE, trim_ws = TRUE)
+df_game <- read_delim("/Users/colinhuliselan/Documents/Master/Seminar/Code/SeminarR/Observations_Game.csv",
+                      ";", escape_double = FALSE, trim_ws = TRUE)
+
+# df_obs <- read_delim("~/Google Drive/Seminar 2020/Data/Observations_Report.csv",
+#                        ";", escape_double = FALSE, trim_ws = TRUE)
+# df_game <- read_delim("~/Google Drive/Seminar 2020/Data/Observations_Game.csv",
+#                       ";", escape_double = FALSE, trim_ws = TRUE)
+
+# Merge to create indices
+df_game$CLICK <- NA
+df <- rbind(df_obs, df_game)
+
+#Combine Mail and Offer id's
+df[,"MailOffer"] <- df %>%
+  unite("MailOffer", c("MAILID" ,"OFFERID"), sep = "_") %>%
+  select("MailOffer")
+
+# Order on click first such that NA are at bottom (no missings indices in training data)
+df <- df[order(df$CLICK), ]
+df <- df %>% 
+  mutate(USERID_ind = group_indices(., factor(USERID, levels = unique(USERID))))
+df <- df %>% 
+  mutate(OFFERID_ind = group_indices(., factor(MailOffer, levels = unique(MailOffer))))
+
+# Make it neat
+# df <- df[order(df$USERID_ind), c("USERID_ind", "OFFERID_ind", "CLICK")]
+df <- df[order(df$USERID_ind), ]
+
+# Create ratios of CLICK per offer or user (== 1 or == 0 indicates no variation)
+df <- df %>%
+  group_by(USERID_ind) %>%
+  mutate(ratioU = mean(CLICK, na.rm = TRUE)) %>%
+  ungroup()
+
+df <- df %>%
+  group_by(OFFERID_ind) %>%
+  mutate(ratioO = mean(CLICK, na.rm = TRUE)) %>%
+  ungroup()
+
+# Split
+df_obs <- df[!(is.na(df$CLICK)), ]
+df_game <- df[is.na(df$CLICK), ]
+
+# Make a results set
+df_obs$res <- rbinom(n = nrow(df_obs), size = 1, prob = 0.2)
+
+# Make a validation set for combining the models
+df_obs$val <- NA
+df_obs$val[df_obs$res == 0] <-  rbinom(n = sum(df_obs$res == 0), size = 1, prob = 0.2)
+
+# Splitting all the sets
+df_res <- df_obs[df_obs$res == 1, ]
+df_val <- df_obs[(df_obs$val == 1 & !is.na(df_obs$val)), ]
+df_train <- df_obs[(df_obs$val == 0 & !is.na(df_obs$val)), ]
+
+# Check if you want
+nrow(df_res) + nrow(df_val) + nrow(df_train) - nrow(df_obs)
+
+
+# Save everything
+saveRDS(df_game, "/Users/colinhuliselan/Documents/Master/Seminar/SharedData/df_game")
+write.csv(df_game, file = "df_game.csv")
+saveRDS(df_obs, "/Users/colinhuliselan/Documents/Master/Seminar/SharedData/df_obs")
+write.csv(df_obs, file = "df_obs.csv")
+saveRDS(df_res, "/Users/colinhuliselan/Documents/Master/Seminar/SharedData/df_res")
+write.csv(df_res, file = "df_res.csv")
+saveRDS(df_val, "/Users/colinhuliselan/Documents/Master/Seminar/SharedData/df_val")
+write.csv(df_val, file = "df_val.csv")
+saveRDS(df_train, "/Users/colinhuliselan/Documents/Master/Seminar/SharedData/df_train")
+write.csv(df_train, file = "df_train.csv")
+
+
+
